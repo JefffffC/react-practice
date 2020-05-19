a7cba1a46f0a60670e3805a3a7260db95777f89a
Jeffrey Chen <44081961+JefffffC@users.noreply.github.com>
2020-05-19 12:39:26 -0400

    starting section 6, debugging react apps

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,17 +1,5 @@
 import React from "react";
-import styled from "styled-components";
-
-const StyledDiv = styled.div`
-  width: 60%;
-  margin: 16px auto;
-  border: 1px solid #eee;
-  box-shadow: 0 2px 3px #CCC;
-  padding: 16px;
-  text-align: center;
-
-  @media (min-width: 500px) {
-  width: 450px;
-`;
+import classes from './Person.css';
 
 const person = (props) => {
   // const style = {
@@ -21,14 +9,13 @@ const person = (props) => {
   // };
 
   return (
-    //<div className="Person" style={style}>
-    <StyledDiv>
+    <div className={classes.Person}>
       <p onClick={props.click}>
         I'm {props.name} and I am {props.age}
       </p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
-    </StyledDiv>
+    </div>
   );
 };
 
